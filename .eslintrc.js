af159b5a71c0cffefe1d9076bef1ebab3fd6130a
Faruq Rasid <me@ruqqq.sg>
2020-02-27 09:49:01 +0800

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:prettier/recommended"
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module',
+        'project': './tsconfig.json'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
