8471c476e26ea1ff0eac14eaedc302cee46be15f
Faruq Rasid <me@ruqqq.sg>
2020-03-03 08:04:28 +0800

    #8 Make entityManager available in context

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { createConnection, getConnectionOptions, ConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions, ConnectionOptions, getManager } from 'typeorm';
 import Telegraf, { ContextMessageUpdate, Middleware } from 'telegraf';
 
 async function setupConnection(typeOrmConnectionOptions?: ConnectionOptions) {
@@ -17,6 +17,8 @@ export async function createApp(
   await setupConnection(typeOrmConnectionOptions);
 
   const bot = new Telegraf(process.env.BOT_TOKEN);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (bot.context as Record<string, any>).entityManager = getManager(dbConnectionName);
   middlewares.forEach(middleware => bot.use(middleware));
 
   return bot;
