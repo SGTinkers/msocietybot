f3a1e08a156dcd2fb2901d6cb9705a6d40701e22
Abdul Aziz <me@ziz.ninja>
2020-03-04 13:11:29 +0800

    #16 WIP: Initial implementation of Reputation Bot

--- src/bots/index.ts
+++ src/bots/index.ts
@@ -3,11 +3,13 @@ import { SceneExampleBot } from './examples/SceneExample';
 import { DebugBot } from './helpers/Debug';
 import { CommonHelper } from './helpers/Common';
 import { ScriberBot } from './Scriber';
+import { ReputationBot } from './Reputation';
 
 export const Bots = {
   CommonHelper,
   DebugBot,
   ScriberBot,
+  ReputationBot,
 };
 
 export const ExampleBots = {
