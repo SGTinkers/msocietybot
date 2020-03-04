fe891ca32322b7997124b903ad56c01dbf7039aa
Abdul Aziz <me@ziz.ninja>
2020-03-04 14:29:54 +0800

    #16 WIP: Updated reputation migration/model

--- src/migration/1583303272429-CreateReputation.ts
+++ src/migration/1583303272429-CreateReputation.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateReputation1583279033786 implements MigrationInterface {
-  name = 'CreateReputation1583279033786';
+export class CreateReputation1583303272429 implements MigrationInterface {
+  name = 'CreateReputation1583303272429';
 
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.query(
-      'CREATE TABLE "reputation" ("id" SERIAL NOT NULL, "value" integer NOT NULL DEFAULT 1, "created_at" TIMESTAMP NOT NULL, "updated_at" TIMESTAMP NOT NULL, "deleted_at" TIMESTAMP, "user_id" bigint, "chat_id" bigint, "message_id" bigint, CONSTRAINT "REL_0ca5aaff74c6eadcf6cc0fdf9f" UNIQUE ("chat_id"), CONSTRAINT "REL_dab63482eb4a70947e0bf27ef8" UNIQUE ("message_id"), CONSTRAINT "PK_640807583e8622e1d9bbe6f1b7b" PRIMARY KEY ("id"))',
+      'CREATE TABLE "reputation" ("id" SERIAL NOT NULL, "value" integer NOT NULL DEFAULT 1, "created_at" TIMESTAMP NOT NULL, "updated_at" TIMESTAMP NOT NULL, "deleted_at" TIMESTAMP, "user_id" bigint, "chat_id" bigint, "message_id" bigint, CONSTRAINT "REL_dab63482eb4a70947e0bf27ef8" UNIQUE ("message_id"), CONSTRAINT "PK_640807583e8622e1d9bbe6f1b7b" PRIMARY KEY ("id"))',
       undefined,
     );
     await queryRunner.query(
