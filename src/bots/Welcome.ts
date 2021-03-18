aa4f60818f34230bf80eccbd848b53abac493f24
Muhd Mirza <muhdmirzamz@gmail.com>
2021-03-18 10:32:32 +0800

    Added finance group link (#57)

--- src/bots/Welcome.ts
+++ src/bots/Welcome.ts
@@ -21,7 +21,8 @@ const WelcomeMessage = {
   1) @MSOCIETYChannel : Up to date announcements on career opportunities and upcoming events
   2) @MSOCIETYIdeas : Idea dumping ground extracted from lobby discussions
   3) @MSOCIETYSoftwareDev : Discussions on Software Dev
-  4) @MSOCIETYML : DIscussions on AI/ML`,
+  4) @MSOCIETYML : DIscussions on AI/ML,
+  5) @MSOCIETYFinance : DIscussions on anything finance`,
   returningMember: 'Welcome back',
 };
 
